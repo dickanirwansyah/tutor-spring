acbdad1773b4a2d63bdbc2be1dcf9108f7043fc0
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2019-01-02 22:33:28 +0700

    soap client

---
+++ soap-client/src/main/java/com/dicka/soapclient/SoapClientApplication.java
@@ -0,0 +1,14 @@
+package com.dicka.soapclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapClientApplication.class, args);
+	}
+
+}
+
