0d7167bde371acfec189ab97193e19dbe903dbb2
dickanirwansyah <dicka.nirwansyah@fusi24.com>
2019-01-03 13:03:53 +0700

    service executor autowired

--- command-pattern/src/main/java/com/dicka/commandpattern/commandPattern/ServiceExecutor.java
+++ command-pattern/src/main/java/com/dicka/commandpattern/commandPattern/ServiceExecutor.java
@@ -2,6 +2,7 @@ package com.dicka.commandpattern.commandPattern;
 
 public interface ServiceExecutor {
 
-	<T, R extends ServiceRequest> T execute(Class<? extends Command<T, R>> commandClass, R request);
+	<T, R extends ServiceRequest> T
+	execute(Class<? extends Command<T, R>> commandClass, R request);
  	
 }
